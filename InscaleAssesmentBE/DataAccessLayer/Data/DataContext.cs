d659d86e83ebdaf0c17d855b999c63505bd3807d
DimovNikola <nikoladimov45@yahoo.com>
2023-03-03 20:14:41 +0100

    added DTOs and seed db

--- InscaleAssesmentBE/DataAccessLayer/Data/DataContext.cs
+++ InscaleAssesmentBE/DataAccessLayer/Data/DataContext.cs
@@ -15,6 +15,30 @@ namespace DataAccessLayer.Data
 
         }
 
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Resource>().HasData(
+                new Resource
+                {
+                    Id = 1,
+                    Name = "Resource X",
+                    Quantity = 100
+                },
+                new Resource
+                {
+                    Id = 2,
+                    Name = "Resource Y",
+                    Quantity = 200
+                },
+                new Resource
+                {
+                    Id = 3,
+                    Name = "Resource Z",
+                    Quantity = 70
+                }
+            );
+        }
+
         DbSet<Resource> Resources { get; set; }
         DbSet<Booking> Bookings { get; set; }
     }
---
+++ InscaleAssesmentBE/DataAccessLayer/Migrations/20230303191150_seedDb.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DataAccessLayer.Migrations
+{
+    public partial class seedDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.InsertData(
+                table: "Resources",
+                columns: new[] { "Id", "Name", "Quantity" },
+                values: new object[] { 1, "Resource X", 100 });
+
+            migrationBuilder.InsertData(
+                table: "Resources",
+                columns: new[] { "Id", "Name", "Quantity" },
+                values: new object[] { 2, "Resource Y", 200 });
+
+            migrationBuilder.InsertData(
+                table: "Resources",
+                columns: new[] { "Id", "Name", "Quantity" },
+                values: new object[] { 3, "Resource Z", 70 });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DeleteData(
+                table: "Resources",
+                keyColumn: "Id",
+                keyValue: 1);
+
+            migrationBuilder.DeleteData(
+                table: "Resources",
+                keyColumn: "Id",
+                keyValue: 2);
+
+            migrationBuilder.DeleteData(
+                table: "Resources",
+                keyColumn: "Id",
+                keyValue: 3);
+        }
+    }
+}
